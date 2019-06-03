ab8441d56954db97564a31242c96a9e81249debe
Vladislav Yarmak <vladislav@vm-0.com>
2019-06-03 15:29:41 +0300

    tests: enable uvloop

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,18 @@
 import asyncio
+import os
 
 import pytest
 from async_generator import yield_, async_generator
 
 from chunk_nordic.splitter import Splitter
 from chunk_nordic.combiner import Combiner
+from chunk_nordic.utils import enable_uvloop
 
 @pytest.fixture(scope="session")
 def event_loop():
+    uvloop_test = os.environ['TOXENV'].endswith('-uvloop')
+    uvloop_enabled = enable_uvloop()
+    assert uvloop_test == uvloop_enabled
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
